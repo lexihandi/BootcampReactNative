5b1f6767655e63a5f67b77cca4a7f591def14d46
lexyhandi <lexihandi@gmail.com>
2020-12-10 09:36:15 +0700

    add task answer to function.js

---
+++ Tugas 4 - Function/function.js
@@ -0,0 +1,22 @@
+//Nomer 1
+console.log("Nomer 1");
+function Teriak() {
+  console.log("Halo Sanbers");
+}
+Teriak();
+console.log("\n");
+
+//Nomer 2
+console.log("Nomer 2");
+function Kalikan(num1, num2) {
+  return num1 * num2;
+}
+console.log(Kalikan(12, 4));
+console.log("\n");
+
+//Nomer 3
+console.log("Nomer 3");
+function Introduce(name, age, address, hobby) {
+  return `Nama saya ${name}, umur saya ${age}, alamat saya ${address}, dan saya punya hobby yaitu ${hobby}`;
+}
+console.log(Introduce("lexy", 22, "malang", "coding"));
