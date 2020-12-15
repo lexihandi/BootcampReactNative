227943de6d2cd85d21fff3756ba0e649d51decfa
lexyhandi <lexihandi@gmail.com>
2020-12-15 14:32:39 +0700

    add number 3

--- Tugas 7 - ES6/es6.js
+++ Tugas 7 - ES6/es6.js
@@ -20,6 +20,15 @@ console.log("\n");
 
 //Nomer 3
 console.log("Nomer 3");
+const newObject = {
+  firstName: "Harry",
+  lastName: "Potter Holt",
+  destination: "Hogwarts React Conf",
+  occupation: "Deve-wizard Avocado",
+  spell: "Vimulus Renderus!!!",
+};
+const { firstName, lastName, destination, occupation, spell } = newObject;
+console.log(firstName, lastName, destination, occupation);
 console.log("\n");
 
 //Nomer 4
