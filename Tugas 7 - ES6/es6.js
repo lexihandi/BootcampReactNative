104a5dbece20c60db5f4225c77230022dc68275b
lexyhandi <lexihandi@gmail.com>
2020-12-15 13:14:27 +0700

    create es6.js

---
+++ Tugas 7 - ES6/es6.js
@@ -0,0 +1,19 @@
+//Nomer 1
+console.log("Nomer 1");
+console.log("\n");
+
+//Nomer 2
+console.log("Nomer 2");
+console.log("\n");
+
+//Nomer 3
+console.log("Nomer 3");
+console.log("\n");
+
+//Nomer 4
+console.log("Nomer 4");
+console.log("\n");
+
+//Nomer 5
+console.log("Nomer 5");
+console.log("\n");
