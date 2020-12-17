bd68aef57ccd7f3a3e8b1fa5b411df19f9e0d853
lexyhandi <lexihandi@gmail.com>
2020-12-17 20:35:38 +0700

    add number 2

--- Tugas 9 - Class/class.js
+++ Tugas 9 - Class/class.js
@@ -48,3 +48,41 @@ console.log("\n");
 
 //Nomer 2
 console.log("Nomer 2");
+class Clock {
+  constructor(template) {
+    this._template = template["template"];
+    this._timer = null;
+  }
+
+  render() {
+    var date = new Date();
+
+    var hours = date.getHours();
+    if (hours < 10) hours = "0" + hours;
+
+    var mins = date.getMinutes();
+    if (mins < 10) mins = "0" + mins;
+
+    var secs = date.getSeconds();
+    if (secs < 10) secs = "0" + secs;
+
+    var output = this._template
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(self._timer);
+  }
+
+  start = function () {
+    this.render();
+    this.timer = setInterval(this.render.bind(this), 1000);
+  };
+}
+
+var clock = new Clock({ template: "h:m:s" });
+clock.start();
