cf4708a222e7b0849fc20220801c40e8e3aab387
lexyhandi <lexihandi@gmail.com>
2020-12-22 12:13:56 +0700

    add tugas12

---
+++ SanberApp/Tugas/Tugas12/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>asd</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
---
+++ SanberApp/Tugas/Tugas12/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import YoutubeUI from './App';
+
+export {YoutubeUI};
