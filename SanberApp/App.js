12b3be579ee15353ea62674e460cc0682184d5c2
lexyhandi <lexihandi@gmail.com>
2020-12-30 10:39:58 +0700

    make folder Quiz3

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -3,7 +3,8 @@ import {YoutubeUI} from './Tugas/Tugas12';
 import {Login, Register, About} from './Tugas/Tugas13';
 import {Todo} from './Tugas/Tugas14';
 import Index from './Tugas/Tugas15/index';
+import Quiz3 from './Tugas/Quiz3/index';
 
 export default function App() {
-  return <Index />;
+  return <Quiz3 />;
 }
